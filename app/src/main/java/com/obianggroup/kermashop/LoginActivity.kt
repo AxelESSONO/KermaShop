b157987dd9f1b08309aa02f1d13a97c357267dba
axel-essono <naxelcdric@gmail.com>
2022-10-01 01:42:02 +0200

    Renamed from SplashScreenActivity.kt

--- app/src/main/java/com/obianggroup/kermashop/LoginActivity.kt
+++ app/src/main/java/com/obianggroup/kermashop/LoginActivity.kt
@@ -3,9 +3,9 @@ package com.obianggroup.kermashop
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SplashScreenActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
